5389d32bef7eb7ea837a15350aeb50affe2654df
dinasooo <82538673+dinasooo@users.noreply.github.com>
2023-03-19 18:23:53 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,20 +1,8 @@
-
-
-// $(document).ready(() =>{
-
-//     $("#crew").on("click", e =>{
-//         let elem = $(e.currentTarget).prev();
-
-//         elem.slideToggle();
-//     })
-// })
-
-//burger
 let burger =document.querySelector('.burger');
 let menu = document.querySelector('.menu');
 let body = document.querySelector('.body');
 
-let links = document.querySelector('.menu__link');
+let links = document.querySelectorAll('.menu__link');
 
 links.forEach(element =>{
     element.addEventListener('click', toggleMenu);
--- js/reviews.js
+++ js/reviews.js
@@ -1,20 +1,40 @@
-const reviewsAvatars = document.querySelector('.review__icons');
+console.log("файл")
 
-reviewsAvatars.addEventListener('click', (e) =>{
+// const reviewsAvatars = document.querySelector('.review__icons');
+
+// reviewsAvatars.addEventListener('click', (e) =>{
+//     e.preventDefault();
+
+//     const target = e.target;
+//     console.log('target', target);
+
+//     if(target.classList.contains('review__icon')){
+//         const activeListItem = document.querySelector('.review__icon--active');
+
+//         if(activeListItem){
+//             activeListItem.classList.remove('.review__icon--active')
+//         }
+
+//         const button = target.parentElement;
+//         const listElemet = button.parentElement;
+        
+//     }
+// });
+const findBlock = (alias) =>{
+    return $(".reviews__item").filter((ndx, item) => {
+        return $(item).attr("data-linked-width") == alias
+    });
+};
+
+$(".reviews__switcher-link").click(e =>{
     e.preventDefault();
 
-    const target = e.target;
-    console.log('target', target);
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-open");
+    const itemToShow = findBlock(target);
+    const curItem = $this.closest(".reviews__switcher-item");
 
-    if(target.classList.contains('review__icon')){
-        const activeListItem = document.querySelector('.review__icon--active');
 
-        if(activeListItem){
-            activeListItem.classList.remove('.review__icon--active')
-        }
-
-        const button = target.parentElement;
-        const listElemet = button.parentElement;
-        
-    }
-});
+    itemToShow.addClass("active").siblings().removeClass("active")
+    curItem.addClass("active").siblings().removeClass("active");
+})
